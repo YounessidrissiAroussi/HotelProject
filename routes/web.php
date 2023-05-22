19249a78f85b4ad941d7317d9e18c3fb77ffc2cb
Youness_Idrissi <youness.2000.idrissi@gmail.com>
2023-05-22 22:06:40 +0100

    modify

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,17 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::controller(ChambersController::class)->group(function () {
+        Route::get('/Chambers', 'index');
+        Route::get('/chambers/create', 'create');
+        Route::get('/chambers/{id}', 'show');
+        Route::get('/chambers/{id}/edit', 'edit');
+        Route::post('/chambers', 'store');
+        Route::patch('/chambers/{id}', 'update');
+        Route::delete('/chambers/{id}', 'destroy');
+    });
+    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+Route::post('register', [RegisteredUserController::class, 'store']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -36,7 +47,7 @@ Route::middleware('auth')->group(function () {
         Route::get('/home', 'index');
     });
     Route::resource('Clients',ClientsController::class);
-    Route::resource('Chambers',ChambersController::class);
+    // Route::resource('Chambers',ChambersController::class);
     Route::resource('Reservations',ReservationsController::class);
     Route::controller(ReservationsController::class)->group(function(){
         Route::patch('/Reservations/{id}/valide', 'valide');
@@ -73,15 +84,7 @@ require __DIR__.'/auth.php';
 //         Route::delete('/Clients/{id}', 'destroy');
 //     });
 
-//     Route::controller(ChambersController::class)->group(function () {
-//         Route::get('/Chambers', 'index');
-//         Route::get('/chambers/create', 'create');
-//         Route::get('/chambers/{id}', 'show');
-//         Route::get('/chambers/{id}/edit', 'edit');
-//         Route::post('/chambers', 'store');
-//         Route::patch('/chambers/{id}', 'update');
-//         Route::delete('/chambers/{id}', 'destroy');
-//     });
+
 
 //     Route::controller(ReservationsController::class)->group(function(){
 //         Route::get('/Reservations', 'index');
